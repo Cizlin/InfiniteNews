e764df08f3b0b47ef36d9be310b050ad69cb1218
Garrisen Cizmich <45015771+Cizlin@users.noreply.github.com>
2023-04-11 13:52:34 -0600

    Create Twitch Drops (Drop ID) page
    Added a dynamic page to view individual Twitch Drops.

---
+++ Page Code/Dynamic Pages/Twitch Drops (Drop ID).js
@@ -0,0 +1,89 @@
+import wixData from 'wix-data';
+
+$w.onReady(function () {
+	$w("#dynamicDataset").onReady(async () => {
+		// We need to manually update the Reward Group Repeater.
+		let datasetItem = $w("#dynamicDataset").getCurrentItem();
+
+		let repeaterArray = [];
+
+		let rewardGroupArray = datasetItem.rewardGroups; // This is the raw reward group array. We need to import some references if they are available.
+		for (let i = 0; i < rewardGroupArray.length; ++i) {
+			let repeaterArrayObject = {};
+			repeaterArrayObject.start = new Date(rewardGroupArray[i].start);
+			repeaterArrayObject.end = new Date(rewardGroupArray[i].end);
+			repeaterArrayObject.minutesToWatch = rewardGroupArray[i].requiredMinutesWatched;
+			repeaterArrayObject.rewardText = "";
+			repeaterArrayObject.imageArray = [];
+
+			repeaterArrayObject._id = i.toString(); // Each object needs a unique ID.
+
+			for (let j = 0; j < rewardGroupArray[i].rewards.length; ++j) {
+
+				let name = rewardGroupArray[i].rewards[j].name;
+				let code = rewardGroupArray[i].rewards[j].code;
+
+				let rewardDefinition = await wixData.query("TwitchDropRewards")
+					.eq("waypointId", code)
+					.contains("title", name)
+					.find()
+					.then((results) => {
+						if (results.items.length > 1) {
+							throw "Too many items returned. Expected 1 and got " + results.items.length;
+						}
+
+						return results.items[0];
+					})
+					.catch((error) => {
+						console.error("Error occurred while fetching Twitch Drop Reward matching this name and code: ", name, code, error);
+						return null;
+					});
+				
+				if (rewardDefinition) {
+					name = rewardDefinition.title;
+					repeaterArrayObject.imageArray = repeaterArrayObject.imageArray.concat(rewardDefinition.imageSet);
+				}
+				
+				repeaterArrayObject.rewardText += name + "\n";
+			}
+
+			repeaterArray.push(repeaterArrayObject);
+		}
+
+		// Assign the array to the repeater
+		$w("#rewardGroupRepeater").data = repeaterArray;
+		console.log(repeaterArray);
+
+		$w("#rewardGroupRepeater").forEachItem(($item, itemData) => {
+			console.log(itemData);
+			// Display the repeater item's data.
+			var options = {
+				month: "short",
+				day: "numeric",
+				year: "numeric",
+				hour: "numeric",
+				minute: "numeric",
+				second: "numeric"
+			};
+
+			$item("#startDateText").text = itemData.start.toLocaleString("en", options);
+			$item("#endDateText").text = itemData.end.toLocaleString("en", options);
+
+			$item("#watchLengthText").text = "Watch for " + itemData.minutesToWatch + " minutes";
+
+			$item("#rewardListText").text = itemData.rewardText;
+
+			// Hide the extra images that we don't need.
+			for (let i = itemData.imageArray.length + 1; i <= 8; ++i) {
+				$item("#image" + i).hide();
+				$item("#image" + i).collapse();
+			}
+
+			for (let i = 0; i < 8 && i < itemData.imageArray.length; ++i) {
+				$item("#image" + (i + 1)).src = itemData.imageArray[i];
+				$item("#image" + (i + 1)).fitMode = "fit";
+			}
+		});
+	});
+
+});
