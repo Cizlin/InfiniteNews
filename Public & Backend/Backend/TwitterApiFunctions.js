df125372cfb26ec91b70592bf45a0d7dd76850cb
Garrisen Cizmich <45015771+Cizlin@users.noreply.github.com>
2023-06-14 13:03:32 -0600

    Moved sendTweet to Twitter v2 to fix v1.1 access issues

--- Public & Backend/Backend/TwitterApiFunctions.js
+++ Public & Backend/Backend/TwitterApiFunctions.js
@@ -1,4 +1,5 @@
 const Twitter = require('twitter');
+const { TwitterApi } = require('twitter-api-v2');
 import {getSecret} from 'wix-secrets-backend';
 import wixMediaBackend from 'wix-media-backend';
 import * as internalNotifications from 'backend/InternalNotificationFunctions.jsw';
@@ -6,27 +7,46 @@ import * as internalNotifications from 'backend/InternalNotificationFunctions.js
 // mediaIds should be comma-separated list of media IDs to include in the Tweet.
 export async function sendTweet(tweetBody, parentId = null, mediaIds = null) {
 	// Generate and send post
-	const CLIENT = new Twitter({
-		consumer_key: await getSecret("TwitterApiKey"),
-		consumer_secret: await getSecret("TwitterApiKeySecret"),
-		access_token_key: await getSecret("TwitterAccessToken"),
-		access_token_secret: await getSecret("TwitterAccessTokenSecret")
-	});
+	let CLIENT;
+
+	const CREDENTIALS = {
+		appKey: await getSecret("TwitterApiKey"),
+		appSecret: await getSecret("TwitterApiKeySecret"),
+		accessToken: await getSecret("TwitterAccessToken"),
+		accessSecret: await getSecret("TwitterAccessTokenSecret")
+	};
+
+	try {
+		CLIENT = new TwitterApi(CREDENTIALS);
+	}
+	catch(error) {
+		console.error(error);
+	}
 
 	//console.log("Sending Tweet with Images...");
 
-	return await CLIENT.post("statuses/update", { status: tweetBody, in_reply_to_status_id: parentId, auto_populate_reply_metadata: true, media_ids: mediaIds })
-		.then((tweet, error, response) => {
-			if (error) {
-				console.error(error);
-				internalNotifications.notifyOwner("Error occurred during Tweet send.", error[0].message);
-			} else {
-				console.log(tweet);
-				return tweet["id_str"];
-			}
+	let options = {};
+
+	if (parentId) {
+		options.reply = {
+			in_reply_to_tweet_id: parentId
+		}
+	}
+
+	if (mediaIds) {
+		options.media = {
+			media_ids: mediaIds
+		}
+	}
+
+	return await CLIENT.v2.tweet(tweetBody, options)
+		.then((tweet) => {
+			console.log(tweet);
+			return tweet.data.id;
 		})
 		.catch(error => {
 			console.error(error);
+
 		});
 }
 
