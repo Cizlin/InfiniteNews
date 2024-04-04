158f6a74546eaae0b61279335b2cb24248ed3717
Garrisen Cizmich <45015771+Cizlin@users.noreply.github.com>
2024-04-03 22:08:00 -0600

    Broke out Twitch notifications into MCC and Infinite sections.

--- Public & Backend/Backend/DiscordBotFunctions.js
+++ Public & Backend/Backend/DiscordBotFunctions.js
@@ -3,7 +3,8 @@ import {getSecret} from 'wix-secrets-backend';
 const CHANNEL_NAME_TO_ID = {
     "shop": "926520859772981288",
     "promotions": "926524772538523668",
-    "twitch-drops": "1102701313218846830",
+    "infinite-twitch-drops": "1102701313218846830",
+    "mcc-twitch-drops": "1102701313218846830",
     "news": "926524570779926558",
     "weekly-reset": "929505647601537056",
     "test": "926520307014049792"
@@ -12,11 +13,13 @@ const CHANNEL_NAME_TO_ID = {
 const CHANNEL_NAME_TO_NOTIF_ROLE = {
     "shop": "941847238085341245",
     "promotions": "941847068434112552",
-    "twitch-drops": "1102702008898687016",
+    "infinite-twitch-drops": "1102702008898687016",
     "news": "941846560814293062",
     "weekly-reset": "941847552167399494",
     "test": "926516155626192988",
-    "new-twitch-drops": "1116078304429088788"
+    "new-infinite-twitch-drops": "1116078304429088788",
+    "mcc-twitch-drops": "1225291679779782686",
+    "new-mcc-twitch-drops": "1225290999165616248"
 };
 
 const discord = require('@jsmrcaga/discord');
@@ -34,7 +37,7 @@ export async function sendDiscordMessage(channel, messageText, notify = false, n
 
     await bot.message({
         channel: CHANNEL_ID,
-        content: messageText + ((notify) ? ("\n<@&" + CHANNEL_NAME_TO_NOTIF_ROLE[channel] + ">") : "") + ((newTwitchDrop) ? ("\n<@&" + CHANNEL_NAME_TO_NOTIF_ROLE["new-twitch-drops"] + ">") : "")
+        content: messageText + ((notify) ? ("\n<@&" + CHANNEL_NAME_TO_NOTIF_ROLE[channel] + ">") : "") + ((newTwitchDrop) ? ("\n<@&" + CHANNEL_NAME_TO_NOTIF_ROLE["new-" + channel] + ">") : "")
     }).then(({ status, headers, response }) => {
         console.log(status);
         console.log(headers);
